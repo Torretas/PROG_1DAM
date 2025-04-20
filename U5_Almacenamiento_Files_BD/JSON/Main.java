2dfb10569a972237f1b688e949475abe555166d9
Alejandro Torres <atorhue617@g.educaand.es>
2025-04-20 14:28:19 +0200

    JSON

---
+++ U5_Almacenamiento_Files_BD/JSON/Main.java
@@ -0,0 +1,29 @@
+package JSON;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Usuario user = new Usuario("Pepe", "pepe@gmail.com");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Convertir el objeto a JSON
+        /*/mapper.writeValue(new File("U5_Almacenamiento_Files_BD/JSON/javausuario.json"), user);
+        System.out.println("Usuario guardado en JSON: " + user);
+*/
+        // Leer el JSON y convertirlo a objeto
+        //Usuario user2 = mapper.readValue(new File("U5_Almacenamiento_Files_BD/JSON/javausuario.json"), Usuario.class);
+        Usuario[] usuarios = mapper.readValue(new File("U5_Almacenamiento_Files_BD/JSON/javausuario.json"), Usuario[].class);
+        for (Usuario u : usuarios) {
+            System.out.println("Usuario leído desde JSON: " + u);
+        }
+
+        //System.out.println("Usuario leído desde JSON: " + user2);
+
+    }
+}
