409e6aa6f6dd60e09694af5a66344794026aeb09
Alejandro Torres <atorhue617@g.educaand.es>
2025-04-27 23:39:25 +0200

    Tema 6 modulo de spring boot

---
+++ U6_Java_Spring_Boot_Microservicios/src/main/java/com/u6_java_spring_boot_microservicios/controller/MyController.java
@@ -0,0 +1,52 @@
+package com.u6_java_spring_boot_microservicios.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+@RestController
+public class MyController {
+
+    // Devuelve texto plano
+    @GetMapping(value = "/texto", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getTexto() {
+        return "Hola, esto es texto plano.";
+    }
+
+    // Devuelve XML
+    @GetMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public String getXml() {
+        return "<mensaje><contenido>Hola, esto es XML</contenido></mensaje>";
+    }
+
+    // Devuelve JSON
+    @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Persona getJson() {
+        return new Persona("Alejandro", "Torres");
+    }
+
+    // Sin utilizar produces, ya detecta el tipo de contenido. De este modo, Spring decide automáticamente qué formato devolver, según la cabecera Accept.
+    @GetMapping("/persona")
+    public Persona getPersona() {
+        return new Persona("Satoru", "Gojo");
+    }
+
+    // Clase interna para representar una Persona
+    static class Persona {
+        private String nombre;
+        private String apellido;
+
+        public Persona(String nombre, String apellido) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getApellido() {
+            return apellido;
+        }
+    }
+}
---
+++ U6_Java_Spring_Boot_Microservicios/src/test/java/com/u6_java_spring_boot_microservicios/U6JavaSpringBootMicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u6_java_spring_boot_microservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6JavaSpringBootMicroserviciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
